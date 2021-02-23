8d43cf671022b00621def908fb6d10ef80c07277
FaizanM2000 <faizanmohammed635@gmail.com>
2021-02-23 17:46:07 -0600

    Update SwingScannerBot.py

--- SwingScannerBot.py
+++ SwingScannerBot.py
@@ -87,8 +87,9 @@ def config(filename = 'database.ini', section = 'postgresql'):
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
+		
             db[param[0]] = param[1]
-	    st.write(params[param])
+	st.write(params)
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
